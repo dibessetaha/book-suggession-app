ae72894480047c42dd7ed8ea47c027084abdb806
DIBESSE <dibesse.taha@etud.univ-pau.fr>
2025-12-17 09:35:52 +0100

    first commit

---
+++ src/main/java/net/cesi/minipro/booksuggestionapp/BookSuggestionAppApplication.java
@@ -0,0 +1,13 @@
+package net.cesi.minipro.booksuggestionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSuggestionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSuggestionAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/cesi/minipro/booksuggestionapp/BookSuggestionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cesi.minipro.booksuggestionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSuggestionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
